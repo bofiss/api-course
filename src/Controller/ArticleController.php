92b2afed5b259c6a6aee6aa5dda7bdfa61e2b0f0
Belle Victor <victor.belle-belle@acadomia.fr>
2019-08-19 07:44:06 +0200

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yuliya
+ * Date: 18/08/2019
+ * Time: 10:22
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\Post;
+
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    private $serializer;
+    private $em;
+    public function __construct(EntityManagerInterface $em , SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/articles", name="article_list")
+     * @Method("GET")
+     * @return Response
+     */
+    public function list(){
+        $articles = $this->em->getRepository(Post::class)->findAll();
+        $data = $this->serializer->serialize($articles, 'json');
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    /**
+     * @Route("/articles/{id}", name="article_show")
+     * @param Post $article
+     * @return Response
+     */
+    public function show(Post $article)
+    {
+
+        $data = $this->serializer->serialize($article, 'json');
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/articles", name="article_create")
+     * @Method("POST")
+     */
+    public function create(Request $request){
+        $data = $request->getContent();
+        $article = $this->serializer->deserialize($data, Post::class, 'json');
+        $this->em->persist($article);
+        $this->em->flush();
+        return new Response('', Response::HTTP_CREATED);
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ * @Serializer\ExclusionPolicy("ALL")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Serializer\Expose
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Serializer\Expose
+     */
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
